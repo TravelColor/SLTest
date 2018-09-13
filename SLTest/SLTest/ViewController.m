f1d36b347a98a9c52c9a3eb40b6d839da062b65d
Travelcolor <978388776@qq.com>
2018-09-13 20:12:06 +0800

    red view

--- SLTest/SLTest/ViewController.m
+++ SLTest/SLTest/ViewController.m
@@ -17,6 +17,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    UIView *view = [[UIView alloc] initWithFrame:CGRectMake(100, 100, -100, -100)];
+    view.backgroundColor = [UIColor redColor];
+    [self.view addSubview:view];
 }
 
 
